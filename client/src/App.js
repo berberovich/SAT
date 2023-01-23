d97b0ea57b667bcaf6836e54832274a7398fc249
garo <garoberebrian1@gmail.com>
2023-01-23 23:36:08 +0400

    dd

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Courses from "./pages/Courses";
+import FAQ from "./pages/FAQ";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="courses" element={<Courses />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+        <Route path="faq" element={<FAQ />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
